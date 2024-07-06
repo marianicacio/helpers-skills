bd09f236ab6cf9482b16cc83feb94ce44b6489b0
Maria Eduarda <mariaeduarda.nicacio2007@gmail.com>
2024-07-06 15:41:07 -0300

    feat: fundamentos

--- src/helpers/fundamentos.js
+++ src/helpers/fundamentos.js
@@ -1,1 +1,12 @@
 console.log("Oi Nerdola")
+
+let error = "Não deu certo Nerdola"
+let info = "Info"
+
+//Métodos pra declarar uma variavel
+console.error("error", error) 
+console.error(`error ${error}`)
+console.info("Nerd", info)
+console.clear("console clean")
+
+console.log("voltei")
